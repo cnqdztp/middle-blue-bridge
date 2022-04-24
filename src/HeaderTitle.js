0dc0566b30265018a3abb4553504fd28b01a3d1c
气泡缓释胶囊 <japanztp@gmail.com>
2022-04-24 11:14:51 +0800

    added UI prompt

--- src/HeaderTitle.js
+++ src/HeaderTitle.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './css/HeaderTitle.css'
+import cloudbase from '@cloudbase/js-sdk';
 // import LogoImg from './image/logo.png'
 
 export default function HeaderTitle() {
   const title = '西街天桥'
-
+  const AuthInfo = cloudbase.auth().hasLoginState();
+  var minified_UID;
+  if(AuthInfo!==null){
+    minified_UID = AuthInfo.user.uid.substring(0,5);
+}
   return (
       <div className="header-title">
         <div className='header_title_text'>
             <h1>
-                {title}
+                {title} #{minified_UID}
             </h1>
         </div>
       </div>
